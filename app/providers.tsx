98c4b9f14619ab87cd2442ea32db1566e72230c7
manish <github.jmtss@gmail.com>
2024-12-25 19:26:37 +0530

    chore : styles

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 
 import { rainbowkitConfig } from "./services/web3";
 
@@ -13,7 +13,18 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={rainbowkitConfig}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
+        <RainbowKitProvider
+          modalSize="compact"
+          theme={darkTheme({
+            ...darkTheme.accentColors.blue,
+            accentColorForeground: "white",
+            borderRadius: "small",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          {children}
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
