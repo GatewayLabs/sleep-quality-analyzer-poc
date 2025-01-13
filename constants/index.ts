af5cd60602cb16ebeb7a12327e943a5c9e77ceee
manish <github.jmtss@gmail.com>
2025-01-13 14:52:39 +0530

    fix: :)

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
 export const WHOOP_BASE_URL = "https://api.prod.whoop.com";
-export const REDIRECT_URI = "http://localhost:3000/api/whoop/callback";
+export const REDIRECT_URI = `${process.env.NEXT_PUBLIC_BASE_URL}/api/whoop/callback`;
